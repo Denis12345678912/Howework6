e29d6d2da518a3cc61caecf0acfc6060695a7d7a
Den <no.den2018@yandex.ru>
2023-07-23 17:18:24 +0500

    Сделали 41 задание

---
+++ task41/Program.cs
@@ -0,0 +1,29 @@
+﻿// Пользователь вводит с клавиатуры N чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+//0, 7, 7, -2, -2 → 2
+
+void GetNumbersGreaterZero(int [] array)
+{
+    int count = 0;
+    for(int i =0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        count++;
+    }
+   Console.Write($"[{String.Join(",", array)}]  ->"); 
+   Console.Write("Положительных чисел -> " + count);
+} 
+
+
+int [] GetArray()
+{
+    int [] array = new int[6];
+    Console.Write("Введите 6 чисел: ");
+    for(int i =0; i < array.Length; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+} 
+
+int [] array = GetArray();
+GetNumbersGreaterZero(array);
